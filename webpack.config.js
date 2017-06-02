03ca6f5a9f7ab1104af0a0070319f4891cd7c80d
Daniel Okwufulueze <daniel.okwufulueze@gmail.com>
2017-06-02 19:53:58 +0100

    master: Make webpack export a JS object

--- webpack.config.js
+++ webpack.config.js
@@ -5,48 +5,46 @@
 
 const path = require('path');
 
-module.exports = [
-  {
-    entry: {
-      'demo': './demo/js/demo.js'
-    },
-    module: {
-      preLoaders: [
-        {
-          test: /\.js$/,
-          exclude: path.join(__dirname, 'node_modules'),
-          loader: 'jshint'
+module.exports = {
+  entry: {
+    'demo': './demo/js/demo.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: path.join(__dirname, 'node_modules'),
+        loader: 'jshint'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: path.join(__dirname, 'node_modules'),
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            'es2015'
+          ]
         }
-      ],
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: path.join(__dirname, 'node_modules'),
-          loader: 'babel-loader',
-          query: {
-            presets: [
-              'es2015'
-            ]
-          }
-        },
-        {
-          test: /\.css$|\.scss$/,
-          exclude: path.join(__dirname, 'node_modules'),
-          loader: 'style!css?url=false!postcss!sass'
-        }
-      ]
-    },
-    output: {
-      path: './demo/js/bundle/',
-      filename: '[name].js'
-    },
-    resolve: {
-      extensions: ['', '.css', '.js', '.scss'],
-      path: __dirname,
-      exclude: path.join(__dirname, 'node_modules')
-    },
-    jshint: {
-      esversion: 6
-    }
+      },
+      {
+        test: /\.css$|\.scss$/,
+        exclude: path.join(__dirname, 'node_modules'),
+        loader: 'style!css?url=false!postcss!sass'
+      }
+    ]
+  },
+  output: {
+    path: './demo/js/bundle/',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.css', '.js', '.scss'],
+    path: __dirname,
+    exclude: path.join(__dirname, 'node_modules')
+  },
+  jshint: {
+    esversion: 6
   }
-];
+};
